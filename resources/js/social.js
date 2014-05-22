0da6d1e5f732b4bf8533192325d399566c580dea
Jack Phoenix <jack@countervandalism.net>
2014-05-23 02:02:04 +0300

    * Stylized * Added typeof check for _gaq variable -- Google Analytics is not always there! * Added missing iPhone/iPad-related variables, which are only used here
    Change-Id: Ic726d3f700cfb3e1f7baae336525b7bbd6cbdf84

--- resources/js/social.js
+++ resources/js/social.js
@@ -1,49 +1,61 @@
-(function ($) {
-	$(document).ready(function() {
+( function ( $ ) {
+	$( document ).ready( function() {
 		WH.addScrollEffectToTOC();
-	});
+	} );
 
-	$(window).load(function() {
-		if ($('.twitter-share-button').length && (!$.browser.msie || $.browser.version > 7)) {
-
-			$.getScript("https://platform.twitter.com/widgets.js", function() {
-				twttr.events.bind('tweet', function(event) {
-					if (event) {
+	$( window ).load( function() {
+		if ( $( '.twitter-share-button' ).length && ( !$.browser.msie || $.browser.version > 7 ) ) {
+			$.getScript( 'https://platform.twitter.com/widgets.js', function() {
+				twttr.events.bind( 'tweet', function ( event ) {
+					if ( event ) {
 						var targetUrl;
-						if (event.target && event.target.nodeName == 'IFRAME') {
-							targetUrl = extractParamFromUri(event.target.src, 'url');
+						if ( event.target && event.target.nodeName == 'IFRAME' ) {
+							targetUrl = extractParamFromUri( event.target.src, 'url' );
 						}
-						_gaq.push(['_trackSocial', 'twitter', 'tweet', targetUrl]);
+						if ( typeof _gaq !== 'undefined' ) {
+							_gaq.push( ['_trackSocial', 'twitter', 'tweet', targetUrl] );
+						}
 					}
-				});
-
-			});
+				} );
+			} );
 		}
 
-		if (isiPhone < 0 && isiPad < 0 && $('.gplus1_button').length) {
+		var ua = navigator.userAgent.toLowerCase(),
+			isiPad = ua.indexOf( 'ipad' ),
+			isiPhone = ua.indexOf( 'iphone' );
+
+		if ( isiPhone < 0 && isiPad < 0 && $( '.gplus1_button' ).length ) {
 			WH.setGooglePlusOneLangCode();
-			var node2 = document.createElement('script');
+			var node2 = document.createElement( 'script' );
 			node2.type = 'text/javascript';
 			node2.async = true;
 			node2.src = 'http://apis.google.com/js/plusone.js';
-			$('body').append(node2);
+			$( 'body' ).append( node2 );
 		}
-		if (typeof WH.FB != 'undefined') WH.FB.init('new');
-		if (typeof WH.GP != 'undefined') WH.GP.init();
 
-		if ($('#pinterest').length) {
-			var node3 = document.createElement('script');
+		// Init Facebook components
+		if ( typeof WH.FB != 'undefined' ) {
+			WH.FB.init( 'new' );
+		}
+
+		// Init Google+ Sign In components
+		if ( typeof WH.GP != 'undefined' ) {
+			WH.GP.init();
+		}
+
+		if ( $( '#pinterest' ).length ) {
+			var node3 = document.createElement( 'script' );
 			node3.type = 'text/javascript';
 			node3.async = true;
 			node3.src = 'http://assets.pinterest.com/js/pinit.js';
-			$('body').append(node3);
+			$( 'body' ).append( node3 );
 		}
 
-		if (typeof WH.imageFeedback != 'undefined') {
+		if ( typeof WH.imageFeedback != 'undefined' ) {
 			WH.imageFeedback();
 		}
-		if (typeof WH.uciFeedback != 'undefined') {
+		if ( typeof WH.uciFeedback != 'undefined' ) {
 			WH.uciFeedback();
 		}
-	});
-})(jQuery);
+	} );
+} )( jQuery );
