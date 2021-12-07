a2e9bd7da4606e6f9448b2947b320e1647d53d30
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:46:54 +0100

    Emit deprecation warning from php entry point
    Change-Id: I021253424d803a0131d3567f57c2dfb1f0a9d7a3

--- BlueSky.php
+++ BlueSky.php
@@ -4,7 +4,11 @@
  * instead. See https://www.mediawiki.org/wiki/Manual:Extension_registration for more details.
  */
 if ( !function_exists( 'wfLoadSkin' ) ) {
-	die( 'The BlueSky skin requires MediaWiki 1.25 or newer.' );
+	die( 'The BlueSky skin requires MediaWiki 1.36 or newer.' );
 }
+wfWarn(
+	'Deprecated PHP entry point used for BlueSky skin. Please use wfLoadSkin instead, ' .
+	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+);
 
 wfLoadSkin( 'BlueSky' );
